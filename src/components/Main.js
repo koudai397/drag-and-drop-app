c0385805b0368032d1beec849d0945539a549ae6
koudai397 <koudai.jaden@gmail.com>
2023-05-12 20:46:45 +0900

    update:タスク一覧を表示させる機能を実装しました

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import dummyData from "../dummyData";
+import Card from "./Card";
 
 const Main = () => {
   const [date, setDate] = useState(dummyData);
@@ -16,6 +17,32 @@ const Main = () => {
                 {...provided.droppableProps}
               >
                 <div className="trello-section-title">{section.title}</div>
+                <div className="trello-section-content">
+                  {section.tasks.map((task, index) => (
+                    <Draggable
+                      key={task.id}
+                      draggableId={task.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            ...provided.draggableProps.style,
+                            opacity: snapshot.isDragging ? "0.5" : "1",
+                            // ドラッグしている途中のDesignを変えることができる
+                          }}
+                        >
+                          <Card>{task.title}</Card>
+                          {/* このtitleはCardコンポーネントのchildrenに挿入される */}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
               </div>
             )}
           </Droppable>
